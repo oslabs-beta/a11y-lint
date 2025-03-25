379b843f79e797838cb9c7e81e96009d74cc8fb1
Brandon <bbeuttel@gmail.com>
2025-03-25 10:45:16 -0700

    started css parsing

--- src/parsers/cssParser.ts
+++ src/parsers/cssParser.ts
@@ -2,6 +2,13 @@
 // FILE: src/parsers/cssParser.ts
 // DESCRIPTION: Uses PostCSS to parse CSS files and applies CSS-specific rules.
 // -----------------------------
+import * as vscode from 'vscode';
+import postcss from 'postcss';
+
+export const cssParse = (cssFile: string) => {
+  const root = postcss.parse(cssFile);
+  
+};
 
 export function parseCSS(code: string, filePath: string) {
   // TODO: Use postcss + cssRules
