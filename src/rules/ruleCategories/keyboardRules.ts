53b8382ab0649a7be553c0f9248489ab941a3332
Chapman <chapman.chappelle@gmail.com>
2025-03-28 12:04:05 -0400

    just added a comment

--- src/rules/ruleCategories/keyboardRules.ts
+++ src/rules/ruleCategories/keyboardRules.ts
@@ -2,4 +2,7 @@ import { Issue } from '../../types/issue';
 
 const keyboardRules: { [key: string]: Function } = {};
 
+// Make sure there is a visible focus style for interactive elements that are navigated to via keyboard input.
+keyboardRules.checkVisibleFocusStyle = () => {};
+
 export default keyboardRules;
