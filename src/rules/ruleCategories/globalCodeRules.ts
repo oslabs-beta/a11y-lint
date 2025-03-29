e7e4340dfc6eaa77155bd58781f12d180df5c716
Chapman <chapman.chappelle@gmail.com>
2025-03-29 11:36:03 -0400

    merge errors

--- src/rules/ruleCategories/globalCodeRules.ts
+++ src/rules/ruleCategories/globalCodeRules.ts
@@ -1,11 +1,11 @@
-import { Issue } from "../../types/issue";
-import { HtmlExtractedNode } from "../../types/html";
+import { Issue } from '../../types/issue';
+import { HtmlExtractedNode } from '../../types/html';
 
 type RuleFunction = (node: HtmlExtractedNode) => Issue | null;
 const globalCodeRules: { [key: string]: RuleFunction } = {
   //************ Test html for missing lang attribute in HTML ****************/
   missingLangAttr: (node) => {
-    if (node.type === "html" && !node.attributes["lang"]) {
+    if (node.type === 'html' && !node.attributes['lang']) {
       return {
         line: node.location?.startLine || 0,
         column: node.location?.startCol,
@@ -13,22 +13,22 @@ const globalCodeRules: { [key: string]: RuleFunction } = {
         endColumn: node.location?.endCol,
         message:
           '<html> tag is missing a "lang" attribute. WCAG Reference [1.3.1], [3.1.1]',
-        severity: "error",
+        severity: 'error',
       };
     }
     return null;
   },
   //************ Test image tag for missing alt attribute ****************/
   missingUniqueTitle: (node) => {
-    if (node.type === "title" && (!node.value || node.value.trim() === "")) {
+    if (node.type === 'title' && (!node.value || node.value.trim() === '')) {
       console.log(node.value);
       return {
         line: node.location?.startLine || 0,
         column: node.location?.startCol,
         endline: node.location?.endLine || 0,
         endColumn: node.location?.endCol,
-        message: "<title> Pages must have a descriptive title",
-        severity: "error",
+        message: '<title> Pages must have a descriptive title',
+        severity: 'error',
       };
     }
     return null;
@@ -36,14 +36,28 @@ const globalCodeRules: { [key: string]: RuleFunction } = {
 
   //************ Test image tag for missing alt attribute ****************/
   missingAltAttr: (node) => {
-    if (node.type === "img" && !node.attributes["alt"]) {
+    if (node.type === 'img' && !node.attributes['alt']) {
       return {
         line: node.location?.startLine || 0,
         column: node.location?.startCol,
         endline: node.location?.endLine || 0,
         endColumn: node.location?.endCol,
         message: '<img> tag is missing an "alt" attribute.',
-        severity: "error",
+        severity: 'error',
+      };
+    }
+    return null;
+  },
+  avoidAutofocus: (node) => {
+    if (node.attributes['autofocus'] || node.attributes['autoFocus']) {
+      return {
+        line: node.location?.startLine || 0,
+        column: node.location?.startCol,
+        endline: node.location?.endLine || 0,
+        endColumn: node.location?.endCol,
+        message: `Avoid using the 'autofocus' attribute. It can cause accessibility issues.`,
+        severity: 'warning',
+        fix: `Remove the 'autofocus' attribute and let users control focus.`,
       };
     }
     return null;
