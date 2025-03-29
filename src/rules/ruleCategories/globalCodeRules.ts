70a3b8adac490ed4db8cb3f5b711ca6ee985ae25
Chapman <chapman.chappelle@gmail.com>
2025-03-29 11:23:46 -0400

    created autofocus rule

--- src/rules/ruleCategories/globalCodeRules.ts
+++ src/rules/ruleCategories/globalCodeRules.ts
@@ -3,21 +3,34 @@ import { HtmlExtractedNode } from '../../types/html';
 
 type RuleFunction = (node: HtmlExtractedNode) => Issue | null;
 const globalCodeRules: { [key: string]: RuleFunction } = {
+  missingLangAttr: (node) => {
+    if (node.type === 'html' && !node.attributes['lang']) {
+      return {
+        line: node.location?.startLine || 0,
+        column: node.location?.startCol,
+        endline: node.location?.endLine || 0,
+        endColumn: node.location?.endCol,
+        message: '<html> tag is missing a "lang" attribute.',
+        severity: 'error',
+      };
+    }
+    return null;
+  },
 
-    missingLangAttr: (node) => {
-        if (node.type === 'html' && !node.attributes['lang']) {
-          return {
-            line: node.location?.startLine || 0,
-            column: node.location?.startCol,
-            endline: node.location?.endLine || 0,
-            endColumn: node.location?.endCol,
-            message: '<html> tag is missing a "lang" attribute.',
-            severity: 'error',
-          };
-        }
-        return null;
-      },
-
+  avoidAutofocus: (node) => {
+    if (node.attributes['autofocus'] || node.attributes['autoFocus']) {
+      return {
+        line: node.location?.startLine || 0,
+        column: node.location?.startCol,
+        endline: node.location?.endLine || 0,
+        endColumn: node.location?.endCol,
+        message: `Avoid using the 'autofocus' attribute. It can cause accessibility issues.`,
+        severity: 'warning',
+        fix: `Remove the 'autofocus' attribute and let users control focus.`,
+      };
+    }
+    return null;
+  },
 };
 
 export default globalCodeRules;
