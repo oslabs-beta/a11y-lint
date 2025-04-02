9f3799e7063e653ddb853511841192299a967885
root <root@laptob.>
2025-04-02 10:24:11 -0500

    updated extension.ts to lint on open and change

--- src/core/extension.ts
+++ src/core/extension.ts
@@ -12,9 +12,9 @@ export function activate(context: vscode.ExtensionContext) {
   vscode.window.showInformationMessage('🔥 A11YLint activated!');
   const diagnostics = vscode.languages.createDiagnosticCollection('a11ylint');
 
-  // Step 1. think of it as an event listener - it is waiting for file saves
+  // Step 1. think of it as an event listener - it is waiting for file to open
   context.subscriptions.push(
-    vscode.workspace.onDidSaveTextDocument((document) => {
+    vscode.workspace.onDidOpenTextDocument((document) => {
       // Step 2. when file is safe it first goes to lintDocument
       const issues = lintDocument(document);
       // Step 5 - converts results into diagnostics
@@ -24,6 +24,14 @@ export function activate(context: vscode.ExtensionContext) {
     })
   );
 
+  //Same as above, but when file is changed
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeTextDocument((document) => {
+      const issues = lintDocument(document.document);
+      const result = toDiagnostics(issues);
+      diagnostics.set(document.document.uri, result);
+    })
+  )
+
   console.log('🥶 A11yLint is now active');
 }
-
--- src/rules/htmlRules.ts
+++ src/rules/htmlRules.ts
@@ -5,26 +5,89 @@
 import { Issue } from '../types/issue';
 import { HtmlExtractedNode } from '../types/html';
 import globalCodeRules from '../rules/ruleCategories/globalCodeRules';
-const allRules = {
-  ...globalCodeRules,
-};
+import imageRules from './ruleCategories/imageRules';
+import controlRules from './ruleCategories/controlRules';
+import formRules from './ruleCategories/formRules';
+import tableRules from './ruleCategories/tableRules';
+import listRules from './ruleCategories/listRules';
+import { cssRulesFromObject } from './cssRules';
+// const allRules = {
+//   ...globalCodeRules,
+// };
 
 
 // step 4- loops through the parsed HTML and applies our rules to them, if something fails, it creates an issue
 export function htmlRules(
-  nodes: HtmlExtractedNode[],
+  parsedJsx: HtmlExtractedNode[],
   filePath: string
 ): Issue[] {
   const issues: Issue[] = [];
 
-  for (const node of nodes) {
-    for (const ruleKey in allRules) {
-      const ruleFn = allRules[ruleKey];
-      const issue = ruleFn(node);
-      console.log(allRules)
-      if (issue) {
-        issues.push(issue);
+  // for (const node of nodes) {
+  //   for (const ruleKey in allRules) {
+  //     const ruleFn = allRules[ruleKey];
+  //     const issue = ruleFn(node);
+  //     console.log(allRules)
+  //     if (issue) {
+  //       issues.push(issue);
+  //     }
+  //   }
+  // }
+
+  for (let i = 0; i < parsedJsx.length; i++) {
+    //want to test all nodes for links EXCEPT <a> tags
+    if (parsedJsx[i].type !== 'a') {
+      controlRules.useATag(parsedJsx[i], issues);
+    }
+    //run in-line styling through CSS parser
+    if (parsedJsx[i].styles) {
+      cssRulesFromObject(parsedJsx[i].styles!, '', issues);
+    }
+    //want to check for potential list structures
+    if (parsedJsx[i].value) {
+      const node: HtmlExtractedNode = parsedJsx[i];
+      const prevValue: HtmlExtractedNode | {value: string} = parsedJsx[i-2] || {value: ' '};
+      const nextValue: HtmlExtractedNode | {value: string}  = parsedJsx[i+2] || {value: ' '};
+      listRules.useListElements(
+        parsedJsx[i],
+        prevValue.value || ' ',
+        nextValue.value || ' ',
+        issues
+      );
+    }
+
+    //if our node is an img tag
+    if (parsedJsx[i].type === 'img') {
+      imageRules.hasAltText(parsedJsx[i], issues);
+    } else if (parsedJsx[i].type === 'a') {
+      controlRules.descriptiveLinks(parsedJsx[i], issues);
+    } else if (parsedJsx[i].type === 'input') {
+      //checks if the previous node is a label for the input
+      formRules.labelInputs(parsedJsx[i], parsedJsx[i - 1], issues);
+      controlRules.useButtonTag(parsedJsx[i], issues);
+    } else if (parsedJsx[i].type === 'form') {
+      formRules.useFieldsetLegend(
+        parsedJsx[i],
+        parsedJsx[i + 1],
+        parsedJsx[i - 1],
+        issues
+      );
+    } else if (
+      parsedJsx[i].type === 'table' &&
+      parsedJsx[i - 1].type !== 'tr'
+    ) {
+      const parsedSlice: HtmlExtractedNode[] = [parsedJsx[i]];
+      //traverses our parsedJsx to find the closing tag for the table
+      let j = i + 1;
+      while (parsedJsx[j].type !== 'table' && j < parsedJsx.length - 1) {
+        parsedSlice.push(parsedJsx[j]);
+        j++;
       }
+      //makes sure that it isn't the closing table tag,
+      // then checks if two nodes after there is a <th> tag
+      // tableRules.hasHeaders(parsedJsx[i], parsedJsx[i - 1], parsedJsx[i + 2], issues);
+      tableRules.hasHeadersBetter(parsedSlice, issues);
+      tableRules.usesCaption(parsedSlice, issues);
     }
   }
   return issues;
