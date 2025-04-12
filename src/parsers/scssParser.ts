37943bba74ab17e3751f33e8b0c870b769076224
Brandon <bbeuttel@gmail.com>
2025-04-11 17:18:03 -0700

    creating parsed outputObject

--- src/parsers/scssParser.ts
+++ src/parsers/scssParser.ts
@@ -6,5 +6,20 @@ import scss from 'postcss-scss';
 export function parseSCSS (code: string, filePath: string): Issue [] {
     const root = scss.parse(code, { from: filePath });
     console.log(root);
-    return [];
+    const selectors: string[] = []
+    const outputObj: CssSelectorObj = {};
+    for (let node of root.nodes) {
+        if (node.type === 'rule') {
+            selectors.push(
+            outputObj[node.selector] = {
+                declarations: {},
+                startLine: source.start.line,
+                startColumn: source.start.column,
+                endLine: source.end.line,
+                endColumn: source.end.column,
+            };
+        )
+    }
+}
+    for ()
 }
