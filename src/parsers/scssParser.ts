ff92e91c0c3a67da98af94e24b3bb9ae22792bec
Brandon <bbeuttel@gmail.com>
2025-04-13 16:08:44 -0700

    completed parsing.  still need to fidure out return

--- src/parsers/scssParser.ts
+++ src/parsers/scssParser.ts
@@ -1,25 +1,40 @@
+import { cssRulesFromObject } from '../rules/cssRules';
 import { Issue } from '../types/issue';
-import { CssSelectorObj } from '../types/css';
+import { SCSSDeclarations, SCSSSelectorObj, Variable } from '../types/scss';
 import postcss from 'postcss';
 import scss from 'postcss-scss';
 
-export function parseSCSS (code: string, filePath: string): Issue [] {
-    const root = scss.parse(code, { from: filePath });
-    console.log(root);
-    const selectors: string[] = []
-    const outputObj: CssSelectorObj = {};
-    for (let node of root.nodes) {
-        if (node.type === 'rule') {
-            selectors.push(
-            outputObj[node.selector] = {
-                declarations: {},
-                startLine: source.start.line,
-                startColumn: source.start.column,
-                endLine: source.end.line,
-                endColumn: source.end.column,
-            };
-        )
+export function parseSCSS(code: string, filePath: string): Issue[] {
+  const root = scss.parse(code, { from: filePath });
+  console.log('SCSS root Object: ', root);
+  const variables: Variable = {};
+  const selectors: SCSSSelectorObj = {};
+  root.walkDecls((decl) => {
+    if (decl.prop.startsWith('$')) {
+      variables[decl.prop] = decl.value;
     }
-}
-    for ()
+  });
+
+  root.walkRules((rule) => {
+    const declarations: SCSSDeclarations = {};
+    rule.walkDecls((decl) => {
+      if (!decl.prop.startsWith('$')) {
+        declarations[decl.prop] = {
+          value: decl.value,
+          startLine: decl.source!.start!.line,
+          startColumn: decl.source!.start!.column,
+          endLine: decl.source!.end!.line,
+          endColumn: decl.source!.end!.column,
+        };
+      }
+    });
+    selectors[rule.selector] = {
+      startLine: rule.source!.start!.line,
+      startColumn: rule.source!.start!.column,
+      endLine: rule.source!.end!.line,
+      endColumn: rule.source!.end!.column,
+      declarations,
+    };
+  });
+  return cssRulesFromObject(selectors, filePath);
 }
