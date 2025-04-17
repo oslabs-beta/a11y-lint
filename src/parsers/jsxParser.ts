b1ef8abb4457b8d7765002e99932f965d7a2ebe4
root <root@laptob.>
2025-04-17 10:52:20 -0500

    commented jsxParser

--- src/parsers/jsxParser.ts
+++ src/parsers/jsxParser.ts
@@ -17,14 +17,17 @@ import * as nodePath from 'path';
 export function parseJSX(code: string, filePath: string): Issue[] {
   let prevNodeType: String = '';
   let results: Node[] = [];
+  //translates the JSX code into an Abstract Syntax Tree
   const ast = parse(code, { sourceType: 'module', plugins: ['jsx'] });
+  //traverses that tree
   traverse(ast, {
     enter(path) {
+      //if a node is an attribute
       if (path.node.type === 'JSXAttribute') {
         const name = String(path.node.name.name);
         const value = String(path.node.value!.value).toLowerCase();
         if (results.length > 0) {
-          //tracking classes and ids
+          //we want to check for className and ID markers so we can feed that info to our dependency graph
           if (name === 'className') {
             value.split(/\s+/).forEach((cls) => {
               addSelectorUsage(`.${cls}`, filePath);
@@ -34,7 +37,8 @@ export function parseJSX(code: string, filePath: string): Issue[] {
             addSelectorUsage(`#${value}`, filePath);
             addClassTagPair(`#${value}`, String(results[results.length - 1].type));
           }
-
+          //we don't need to do anything special with non-"style" attributes, just store them in the last object in our array
+          //(which will be the most recently parsed node)
           if (name !== 'style') {
             results[results.length - 1].attributes[name] = {
               value: value,
@@ -46,12 +50,17 @@ export function parseJSX(code: string, filePath: string): Issue[] {
               },
             };
           } else {
+            //for "style" attributes, we need to parse them into CSS so we can run rules on them
+            //to do this, we split the string along semi-colons
             let values: String[] = value.split(';');
             let declarations: Declarations = {};
+            //starts 8 characters from the beginning of the style attribute, which accounts for the five letters, the equal sign, and the opening
+            //quotation mark
             let addLineStart: number = 8;
             const selector: string = String(results[results.length - 1].type);
             const selectorLocation = results[results.length - 1].location;
             const location = path.node.loc;
+            //this allows us to get declaration name and precise location info for each declaration in the style attribute
             for (let i = 0; i < values.length - 1; i++) {
               const decPair: string[] = values[i].trim().split(':');
               const addLineEnd: number = decPair[0].length + decPair[1].length;
@@ -64,6 +73,7 @@ export function parseJSX(code: string, filePath: string): Issue[] {
               };
               addLineStart += addLineEnd + 3;
             }
+            //and we add all that info to the "style" attribute on the node's result object
             results[results.length - 1].styles = {};
             results[results.length - 1].styles![selector] = {
               declarations: declarations,
@@ -73,6 +83,7 @@ export function parseJSX(code: string, filePath: string): Issue[] {
               endColumn: selectorLocation.endColumn,
             };
         }}}
+        //this just stores the tag name and location for each JSX element in our output array
        else if (
         path.node.type === 'JSXOpeningElement'
       ) {
@@ -86,6 +97,7 @@ export function parseJSX(code: string, filePath: string): Issue[] {
           },
           attributes: {},
         });
+        //text is stored as the value of whatever node it belongs to
       } else if (
         path.node.type === 'JSXText' &&
         path.node.value.charAt(0) !== '\n'
